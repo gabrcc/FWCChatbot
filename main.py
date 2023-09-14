be01b0646c88e6c872c3f840bd1be03b2f2f6b6a
gabrcc <cuellar.caero.gabriela@gmail.com>
2023-09-14 18:29:11 -0400

    api key modified using env, addition tests

--- main.py
+++ main.py
@@ -3,18 +3,18 @@ from langchain.document_loaders import PyPDFLoader
 from langchain.document_loaders import CSVLoader
 import os
 import openai
-from dotenv import load_dotenv, find_dotenv
+from dotenv import load_dotenv
 from tqdm import tqdm
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
+from langchain.llms import OpenAI
 
-os.environ["OPEN_API_KEY"] = ""
-_ = load_dotenv(find_dotenv())
-openai.api_key = os.environ['OPEN_API_KEY']
 
+load_dotenv()
+OpenAI.api_key = os.getenv("OPENAI_API_KEY")
 ##########################################################
-#load pdfs
+
 def LoadPDF(path):
     loader = DirectoryLoader(path, glob="**/*.pdf", show_progress=True, loader_cls=PyPDFLoader)
     return loader.load()
@@ -32,13 +32,12 @@ csv_data_list = []
 csv_files = [os.path.join(csv_directory, file) for file in os.listdir(csv_directory) if file.endswith(".csv")]
 #tqdm show progress bar
 for csv_file in tqdm(csv_files):
-    csv_data = LoadCSV(csv_file)
-    csv_data_list.append(csv_data)
+    csv_data1 = LoadCSV(csv_file)
+    csv_data_list.append(csv_data1)
 
 #######################################################
 #LOAD Directory inside another directory
 def load_csv_directory(directory_path):
-    csv_data_list = []
     
     # Go through all general directory
     for root, dirs, files in os.walk(directory_path):
@@ -51,17 +50,24 @@ def load_csv_directory(directory_path):
                 csv_data = LoadCSV(csv_file_path)
                 csv_data_list.append(csv_data)
     
-    return csv_data_list
+    return csv_data
 
 general_directory = "fifa_wc_2018"
 
 # Load CSV from the subdirectories 
-load_csv_directory(general_directory)
+csv_data2=load_csv_directory(general_directory)
+csv_data_list.append(csv_data2)
 ############################################################
 
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
 splitted = text_splitter.split_documents(loaded_pdfs)
 ########################################################
-# embeddings = OpenAIEmbeddings()
-# vectorstore = Chroma.from_documents(loaded_pdfs, embeddings)
+chroma_directory="chroma"
+all_data=[]
+all_data.extend(splitted)
+all_data.extend(csv_data1)
+all_data.extend(csv_data2)
+embeddings = OpenAIEmbeddings()
+vectorstore = Chroma.from_documents(documents=all_data,embedding=embeddings,persist_directory=chroma_directory)
+vector=Chroma(persist_directory=chroma_directory, embedding_function=embeddings)
 
